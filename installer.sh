794634a7c6c9f13687009eceea5a2d11e9374dbb
bent <bent@bentubuntu.com>
2022-02-23 23:13:32 +0100

    Skip Setup Wizard 1.0

--- installer.sh
+++ installer.sh
@@ -3,10 +3,10 @@
 case $1 in
 
 redhat)
-# This is where we install Ansible for Redhat
+# This is where we install Ansible and git for Redhat
 sudo yum update
 sudo yum install epel-release && sudo yum install ansible
-
+sudo yum install git
 # Now we install terraform for redhat, so we can automaticly setup a remote tomcat server on aws later, this is easier in terraform than in ansible.
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
@@ -15,11 +15,12 @@ sudo yum -y install terraform
 
 
 ubuntu)
-# This is where we install ansible for ubuntu
+# This is where we install ansible and git for ubuntu
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+sudo apt install git
 
 ## Now we install Terraform in Ubuntu so we can create a tomcat aws instance easily later.
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
