486f85d18f522f0b31c76c6cc474b6169673b999
ubuntu18.04 <cloudctrlschagen@gmail.com>
2022-02-23 18:09:52 -0500

    Bypass

---
+++ init.groovy.d/basic-security.groovy
@@ -0,0 +1,17 @@
+#!groovy
+
+
+
+import jenkins.model.*
+
+import hudson.util.*;
+
+import jenkins.install.*;
+
+
+
+def instance = Jenkins.getInstance()
+
+
+
+instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
