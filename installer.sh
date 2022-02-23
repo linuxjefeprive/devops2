810f61aba775e4888a3b38d81083134033c1736f
bent <bent@bentubuntu.com>
2022-02-23 23:27:41 +0100

    Fix -y

--- installer.sh
+++ installer.sh
@@ -6,21 +6,21 @@ redhat)
 # This is where we install Ansible and git for Redhat
 sudo yum update
 sudo yum install epel-release && sudo yum install ansible
-sudo yum install git
+sudo yum install git -y
 # Now we install terraform for redhat, so we can automaticly setup a remote tomcat server on aws later, this is easier in terraform than in ansible.
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install terraform
+sudo yum -y install -y terraform
 ;;
 
 
 ubuntu)
 # This is where we install ansible and git for ubuntu
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common 
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
-sudo apt install git
+sudo apt install -y ansible
+sudo apt install -y git
 
 ## Now we install Terraform in Ubuntu so we can create a tomcat aws instance easily later.
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
