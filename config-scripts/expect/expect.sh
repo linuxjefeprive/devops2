e2ed41baecacf032ffa025865fc29508e5384978
Your Name <you@example.com>
2022-02-25 12:54:48 -0500

    Update Config Scripts

---
+++ config-scripts/expect/expect.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/expect
+
+set password [lindex $argv 0]
+set HOME [lindex $argv 1]
+
+spawn ansible-playbook $HOME/devops2/playbooks/jenkins-config.yaml
+
+expect "be?:" { send "$password\r" }
+
+interact
