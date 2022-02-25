410df39fc47eb6257c3970b8f2c80623591c1959
Your Name <you@example.com>
2022-02-25 14:07:37 -0500

    Fix

--- config-scripts/expect/expect.sh
+++ config-scripts/expect/expect.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/expect
 
+set timeout 240
 set password [lindex $argv 0]
 set HOME [lindex $argv 1]
 
 spawn ansible-playbook $HOME/devops2/playbooks/jenkins-config.yaml
 
 expect "be?:" { send "$password\r" }
+expect "Are you sure you want to continue connecting (yes/no)? " { send "yes\r" }
 
 interact
